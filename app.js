675e877b610bff23b4ffab4eeff3809c3e1a33d6
Juan Arias <jcamiloat@gmail.com>
2022-10-04 14:16:27 -0500

    Ex 4.1 Blog List step02

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
 const mongoose = require('mongoose');
 const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
 
 const mongoUrl = config.MONGODB_URI;
 mongoose
@@ -14,7 +15,11 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;

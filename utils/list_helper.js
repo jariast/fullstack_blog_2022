6ce8c277b350b5b130cc1c8932c3e1eb56c67bcb
Juan Arias <jcamiloat@gmail.com>
2022-10-05 11:57:22 -0500

    Ex 4.5 Helper functions step03

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,7 +7,20 @@ const totalLikes = (blogs) => {
   return blogs.reduce((accum, blog) => accum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((fav, blog) => compareLikes(fav, blog), null);
+};
+
+const compareLikes = (currentFav, blog) => {
+  if (!currentFav) {
+    return blog;
+  } else {
+    return blog.likes > currentFav.likes ? blog : currentFav;
+  }
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };

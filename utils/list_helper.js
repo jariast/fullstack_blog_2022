150c3ae56c1ce53605be8d449fe7fda94ff18ff2
Juan Arias <jcamiloat@gmail.com>
2022-10-05 10:48:44 -0500

    Ex 4.4 Helper functions step02

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,13 @@
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((accum, blog) => accum + blog.likes, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };

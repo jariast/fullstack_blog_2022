5865286ab04d388834c097b8d9bbdd28992d0186
Camilo Arias <jcamiloat@gmail.com>
2022-10-22 19:00:06 -0500

    chore: Fixing testing EndPoint.

--- controllers/testing.js
+++ controllers/testing.js
@@ -7,7 +7,7 @@ testingRouter.post('/reset', async (request, response) => {
   await Blog.deleteMany({});
   await User.deleteMany({});
 
-  response.status(204).end;
+  response.status(204).end();
 });
 
 module.exports = testingRouter;
